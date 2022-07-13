c40485550979cee34d225277c115033c1d18f348
Amakthel <Amakthel@protonmail.ch>
2022-07-13 11:57:02 -0400

    to shrink points on the scatter plot

--- plot_clust.py
+++ plot_clust.py
@@ -6,16 +6,11 @@ feature_set = np.load('./script_data/reduced.npy')
 
 fig, axes = plt.subplots(1, 3)
 
-axes[0].scatter(feature_set[:, 0], feature_set[:, 1], s=20, c=clusters, cmap='gist_rainbow')
-axes[0].set_xlabel('FSC-A')
-axes[0].set_ylabel('SSC-A')
-
-axes[1].scatter(feature_set[:, 1], feature_set[:, 2], s=20, c=clusters, cmap='gist_rainbow')
-axes[1].set_xlabel('SSC-A')
-axes[1].set_ylabel('FL1-A')
-
-axes[2].scatter(feature_set[:, 2], feature_set[:, 0], s=20, c=clusters, cmap='gist_rainbow')
-axes[2].set_xlabel('FL1-A')
-axes[2].set_ylabel('FSC-A')
+for n in range(np.shape(axes)[0]):
+    axes[n].scatter(feature_set[:, n],
+                    feature_set[:, (n+1)%3],
+                    s=1,
+                    c=clusters,
+                    cmap='gist_rainbow')
 
 plt.savefig('./plots/cluster_plots.png')
