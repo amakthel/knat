312a3e01a54a3d37ee71aad05a60f4a1baa89424
Amakthel <Amakthel@protonmail.ch>
2022-07-13 13:59:51 -0400

    concatenate the three scripts into one

--- README.md
+++ README.md
@@ -3,24 +3,7 @@
 attempting to use machine learning do identify useful sub-populations
 in flow cytometry data on K. nataicola bacteria.
 
-Currently the code is seperated into three parts: link_to_clust.py is
-depreciated now that we use scikit-learn. save_and_scale.py pulls flow
-cytometry data from an .fcs file, transforms it to rfi, scales it down
-with sinh scaling, trims off less important channels, and then saves
-both a .csv and a .npy version of the data. agglom.py takes the .npy,
-loads it, creates an AgglomerativeClustering object to handle it, and
-fits that object to the data. It produces a numpy array of the clusters
-to which each observation is assigned, and saves that to clust.npy.
-plot_clust.py plots the data, highlighting the clusters with the dark2
-color palette.
-
-## Getting started
-
-To make it easy for you to get started with GitLab, here's a list of
-recommended next steps.
-
-Already a pro? Just edit this README.md and make it your own. Want to
-make it easy? [Use the template at the bottom](#editing-this-readme)!
+Currently the code is constructed as a single script: link_to_clust.py, save_and_scale.py, agglom.py, and plot_clust.py are depreciated now that we use scikit-learn and check a variety of numbers of clusters. subdivide_fcs.py pulls flow cytometry data from an .fcs file, transforms it to rfi, scales it down with sinh scaling, trims off less important channels, creates an AgglomerativeClustering object to handle the new feature set, and fits that object to the data. It produces a numpy array of the clusters to which each observation is assigned, and plots the data, highlighting the clusters with the dark2 color palette.
 
 ## Add your files
 
---
+++ subdivide_fcs.py
@@ -0,0 +1,78 @@
+import FlowCal
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import cluster
+from scipy.cluster import hierarchy
+
+bead_file_name = 'beads-1.fcs'
+bead_path = '../beads/'+bead_file_name
+
+try:
+    b = FlowCal.io.FCSData(bead_path)
+except FileNotFoundError:
+    print('there is no file at '
+        + bead_path
+        + ' . make sure that you have the correct path')
+except Exception:
+    print('something unexpected went wrong while trying to process the'
+          ' .fcs file. email iabenjamin@wpi.edu with this error.')
+else:
+    print('flow cytometry data successfully drawn from bead file')
+
+# Removes first 250 and last 100 events,
+# used in FlowCal excel_ui script
+b = FlowCal.gate.start_end(b, num_start=250, num_end=100)
+# this converts the raw voltage values for FL1-A (GFP-A) to
+# Relative Fluorescent Intensity (AU)
+b_transformed = FlowCal.transform.to_rfi(b, channels='FL1-A')
+print('flow cytometry data successfully transformed to Relative '
+      'Fluorescent Intensity')
+
+# FSCData type arrays have additional features compared to numpy arrays.
+# A non-exhaustive list:
+# names for the various columns associated with a
+# particular channel on the flow cytometer
+# a hidden range variable for each column that keeps track of the
+# interval occupied by the data in that column
+#
+# Because of the second of the above two features, when scaling a
+# FSCData array, you need to use FlowCal.transform.transform so that the
+# range is scaled along with the array data. In general try to use
+# FlowCal methods and functions on FSCData objects because functions
+# meant for numpy arrays can occasionally remove the channel and range
+# data.
+def sinhScaling(x):
+    return np.arcsinh(x)/np.log(10)
+b_scaled = FlowCal.transform.transform(b_transformed,
+                                       channels=None,
+                                       transform_fxn=sinhScaling)
+b_scaled = FlowCal.transform.transform(b_scaled,
+                                       channels=None,
+                                       transform_fxn=lambda x: x/8)
+print('flow cytometry data successfully scaled')
+# all other features of the data set are either enormously correlated
+# or very low variance and are  therefore not needed for the analysis
+feature_set = b_scaled[:, ['FSC-A', 'SSC-A', 'FL1-A']]
+print('features successfully trimmed from the flow cytometry data')
+for cluster_number in range(3:16):
+    agglo = cluster.AgglomerativeClustering(n_clusters=cluster_number)
+    print('agglomerative clustering object ({:n} clusters) created'\
+            .format(cluster_number))
+    with cluster_assignment as agglo.fit_predict(feature_set):
+        print('feature set analyzed into {:n} clusters'\
+                .format(cluster_number))
+        fig, axs = plt.subplots(1, 3, figsize=[14.4, 4.8])
+        for n in range(np.shape(axs)[0]):
+            axs[n].scatter(feature_set[:, n],
+                            feature_set[:, (n+1)%3],
+                            s=1,
+                            c=cluster_assignment,
+                            cmap='gist_rainbow')
+        axs[0].set_xlabel('FSC-A')
+        axs[0].set_ylabel('SSC-A')
+        axs[1].set_xlabel('SSC-A')
+        axs[1].set_ylabel('FL1-A')
+        axs[2].set_xlabel('FL1-A')
+        axs[2].set_ylabel('FSC-A')
+        plt.savefig('./plots/cluster_plots{:n}.png'.format(number_of_clusters))
