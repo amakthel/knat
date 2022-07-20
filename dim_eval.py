58914452eabdc4f2a875c3ad04cc9eaca0a3239a
Amakthel <Amakthel@protonmail.ch>
2022-07-20 12:20:47 -0400

    update dim_eval.py

--- dim_eval.py
+++ dim_eval.py
@@ -54,8 +54,51 @@ variances = np.zeros(number_of_channels)
 for i in range(number_of_channels):
     variances[i] = st.variance(pre[:, i])
 
+np.save(vari_path, variances)
+
+print(variances)
+print('we remove the '
+        + channel_names[16]
+        + ' and '
+        + channel_names[17]
+        + ' channels as their variances are extremely low')
+
+pre = pre[:, 0:16]
 corr_coeffs = np.corrcoef(pre, rowvar=False)
 
-np.save(vari_path, variances)
 np.save(corr_path, corr_coeffs)
 
+low = ''
+moderate = ''
+tolerable = ''
+for i in range(np.shape(corr_coeffs)[0]):
+    for j in range(np.shape(corr_coeffs)[1]):
+        corr = corr_coeffs[i][j]
+        if corr < 0.5:
+            low = (low
+                    +'('
+                    + channel_names[i]
+                    + ', '
+                    + channel_names[j]
+                    + ')\n')
+        if corr < 0.55:
+            moderate = (moderate
+                        + '('
+                        + channel_names[i]
+                        + ', '
+                        + channel_names[j]
+                        + ')\n')
+        if corr < 0.6:
+            tolerable = (tolerable
+                        + '('
+                        + channel_names[i]
+                        + ', '
+                        + channel_names[j]
+                        + ')\n')
+
+print('those pairs of channels with a correlation of 0.5 or less:\n'
+        + low)
+print('those pairs of channels with a correlation of 0.55 or less:\n'
+        + moderate)
+print("those pairs of channels with a correlation of 0.6 or less"
+        + tolerable)
